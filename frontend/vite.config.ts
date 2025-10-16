11abf889c26bdbf695a05d534adb4a5ad23d6e72
3zequiel3 <3zegonzalez@gmail.com>
2025-10-16 14:12:57 +0000

    Soluciones en errores de configuracion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,8 +10,9 @@ export default defineConfig({
         index: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "src/pages/auth/login/login.html"),
         register: resolve(__dirname, "src/pages/auth/register/register.html"),
-        adminHome: resolve(__dirname, "src/pages/admin/home/home.html"),
-        clientHome: resolve(__dirname, "src/pages/client/home/home.html"),
+        adminHome: resolve(__dirname, "src/admin/home/home.html"),
+        // clientHome: resolve(__dirname, "src/client/home/home.html"),
+        clientOrder: resolve(__dirname, "src/client/orders/orders.html"),
 
       },
     },
